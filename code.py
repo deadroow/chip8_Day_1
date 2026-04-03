47530cd472c3033c5c2e90f68181ea0af42db286
Evan K <evan.knockaert@edu.essie-it.fr>
2026-04-03 09:36:59 +0200

    debut code

---
+++ code.py
@@ -0,0 +1,132 @@
+import pygame
+import sys
+from random import randint
+
+sprite=[
+    0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, # 0 et 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, 0xF0, 0x10, 0xF0, 0x10, 0xF0, # 2 et 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0, # 4 et 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, 0xF0, 0x10, 0x20, 0x40, 0x40, # 6 et 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0, 0x10, 0xF0, # 8 et 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0, # A et B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0, # C et D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80  # E et F
+]
+
+class chip_8:
+    def __init__(self):
+        self.v=[0]*16
+        self.memoir=[0]*4096
+        self.pc=0x200
+        self.I=0
+        self.stack=[0]*16
+        self.SP=0
+        self.ecran=[0]*(64*32)
+        self.delay_timer=0
+        self.sound_timer=0
+
+        for i in range(len(sprite)):
+            self.memoir[i]=sprite[i]
+
+        
+    def cycle (self):
+        opcode=(self.memoir[self.pc]<<8) | self.memoir[self.pc]
+        t=(opcode&0xF000)>>12
+        x=(opcode&0x0F00)>>8
+        y=(opcode&0x00F0)>>4
+        N=(opcode&0x000F)
+        NN=(opcode&0x00FF)
+        NNN=(opcode&0x0FFF)
+
+        match t: # trouve la commande a exécuter
+            case 0x0: # reset le tab
+                if NN==0xE0:
+                    self.ecran=[0]*(64*32)
+                elif nn==0xEE: #sort d'une sous routine
+                    self.SP-=1
+                    self.pc=self.stack[self.SP] 
+            case 0x1:
+                self.pc=NNN
+            case 0x2: # régle pc a l'adresse NNN et sauvegarde son ancien adresse dans stack incrémente SP
+                self.stack[self.SP]=self.pc
+                self.SP+=1
+                self.pc=NNN
+            case 0x3:# si vx == NN saute instruction
+                if self.v[x]==NN:
+                    self.pc+=2
+            case 0x4:# si vx diff de NN saute instruction
+                if self.v[x]!=NN:
+                    self.pc=NN
+            case 0x5: #si vx == vy saute instruction 
+                if self.v[x]==self.v[y]:
+                    self.pc+=2
+            case 0x6: # donne la valeur NN a V[x]
+                self.v[x]=NN
+            case 0x7: # rajoute NN a vx et mettre un modulo 256 sur vx
+                self.v[x]=(self.v[x]+NN) & 0xFF
+            case 0x8:
+                match N:
+                    
+                    case 0x0:# stock vy dans vx
+                        self.v[x]=self.v[y]
+                    
+                    case 0x1:# vx stocke le or de vx et vy 
+                        self.v[x]=self.v[x]|self.v[y]
+                    
+                    case 0x2: # result and vx et vy sauv dans vx
+                        self.v[x]=self.v[x] & self.v[y]
+                    
+                    case 0x3: # vx= vx xor vy
+                        self.v[x]=self.v[x] ^self.v[y]
+                    
+                    case 0x4: # ADD Vx, Vy
+                        somme = self.v[x] + self.v[y]   
+                        # 1. On vérifie s'il y a un dépassement (> 255)
+                        if somme > 0xFF:
+                            retenue = 1
+                        else:
+                            retenue = 0
+                        # 2. On applique le résultat sur 8 bits
+                        self.v[x] = somme & 0xFF
+                        # 3. On met à jour VF à la toute fin
+                        self.v[0xF] = retenue
+                    
+                    case 0x5: # vx=vx-vy
+                        if self.v[x]<self.v[y]:
+                            s=0
+                        else:
+                            s=1
+                        self.v[x]=(self.v[x]-self.v[y])&0xFF
+                        self.v[0xF]=s
+                    case 0x6: # si dernier bit de vx == 1 alors vf=1 sinon vf=0 dans tout les cas apres div vx//2 décalage a droite
+                        self.v[0xF]=self.v[x]&0x01
+                        self.v[x]=self.v[x]>>1
+                    
+                    case 0x7: # vy - vx
+                        if self.v[y]>self.v[x]:
+                            self.v[0xF]=1
+                        else:
+                            self.v[0xF]=0
+                        self.v[x]=(self.v[y]-self.v[x])&0xFF
+                    
+                    case 0xE:# décalage a gauche
+                        self.v[0xF]=(self.v[x]&0x80)>>7
+                        self.v[x]= (self.v[x]<<1) & 0xFF
+
+                    case _:
+                        print(f"instruction non gérée : {hex(opcode)}")
+            case 0x9: # saute instruction si x!=y
+                if self.v[x]!=self.v[y]:
+                    self.pc+=2
+            case 0xA: # I=NNN
+                self.I=NNN
+            case 0xB: # jump a l'addresse nnn+vo
+                self.pc=NNN+self.v[0]
+            case 0xC:
+                self.v[x]=(randint(0,255)& NN)
+
+            case 0xd: # dessin
+                x_pos=self.v[x]%64
+                y_pos=self.v[y]%32
+                self.v[0xF]=0
+           
